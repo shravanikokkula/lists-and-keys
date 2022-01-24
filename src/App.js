96c6effbeb23ab9f6198dd34d76f80576e2d1a53
shravani <shravanikokkula67@gmail.com>
2022-01-24 09:59:04 +0000

    lists-keys solutions

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import UserProfile from './components/UserProfile/index'
+import './App.css'
+
+const userDetails = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Rahul',
+    role: 'Software developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Rahul',
+    role: 'Software developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Rahul',
+    role: 'Software developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Rahul',
+    role: 'Software developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetails.map(eachItem => (
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
